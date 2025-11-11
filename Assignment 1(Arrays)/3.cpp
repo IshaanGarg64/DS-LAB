ae55829c478417db91cc791038deafa45b6214c9
IshaanGarg64 <gargishaan06@gmail.com>
2025-11-11 10:44:37 +0530

    Add C++ program for array output prediction
    Add a C++ program to predict output of array initialization.

---
+++ Assignment 1(Arrays)/3.cpp
@@ -0,0 +1,12 @@
+/* Predict the Output of the following program */
+
+#include <iostream>
+using namespace std;
+int main(){
+int i;
+int arr[5] = {1};
+for (i = 0; i < 5; i++)
+printf("%d",arr[i]);  // 10000
+return 0;
+}
+
