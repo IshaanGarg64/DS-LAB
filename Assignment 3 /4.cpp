c4f3a66d7317443809a116f537c6d332f1dfdcb3
IshaanGarg64 <gargishaan06@gmail.com>
2025-11-13 10:05:55 +0530

    Create 4.cpp

---
+++ Assignment 3 /4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int prec(char c) {
+    if(c == '^') return 3;
+    if(c == '*' || c == '/') return 2;
+    if(c == '+' || c == '-') return 1;
+    else return -1;
+}
+string infixToPostfix(string s) {
+    stack<char> st;
+    string res;
+
+    for(int i=0;i<s.length();i++) {
+        if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z' || s[i]>='0' && s[i]<='9') {
+            res += s[i];
+        }
+        else if(s[i] == '(') {
+            st.push(s[i]);
+        }
+        else if(s[i] == ')') {
+            while(!st.empty() && st.top() != '(') {
+                res += st.top();
+                st.pop();
+            }
+            if(!st.empty()) st.pop();
+        }
+        else {
+            while(!st.empty() && prec(st.top()) > prec(s[i]) || 
+                 (!st.empty() && prec(st.top()) == prec(s[i]) && s[i] != '^')) {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()) {
+        res += st.top();
+        st.pop();
+    }
+    return res;
+}
+int main(){
+    string s;
+    cout<<"Enter Infix Expression : ";
+    getline(cin, s);
+
+    string result = infixToPostfix(s);
+    cout<<"Postfix Expression is : "<<result<<endl;
+
+    return 0;
+}
