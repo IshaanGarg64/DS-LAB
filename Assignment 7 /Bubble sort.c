44537f101094de5f220b229ab7e4c4a9ecb20c2e
IshaanGarg64 <gargishaan06@gmail.com>
2025-11-27 10:46:27 +0530

    Add bubble sort implementation in Bubble sort.c
    Implement bubble sort algorithm to sort an array of integers and print the sorted array.

---
+++ Assignment 7 /Bubble sort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int arr[6]={45,3,31,5,35,6};
+    int n=6;
+    int i,j,temp;
+    
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }    
+}
